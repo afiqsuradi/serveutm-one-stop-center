d5563e99661c57ca2763b561fbdebf6eb3f749e0
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-26 22:02:03 +0800

    Create Login Form

---
+++ src/client/src/components/Login/LoginForm.tsx
@@ -0,0 +1,48 @@
+const LoginForm = () => {
+  return (
+    <form className="flex flex-col" method="POST" action="#">
+      <div className="mb-6 pt-3 rounded bg-gray-200">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2 ml-3"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          id="email"
+          className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+        />
+      </div>
+      <div className="mb-6 pt-3 rounded bg-gray-200">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2 ml-3"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+        />
+      </div>
+      <div className="flex justify-end">
+        <a
+          href="#"
+          className="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6"
+        >
+          Forgot your password?
+        </a>
+      </div>
+      <button
+        className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+        type="submit"
+      >
+        Sign In
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
