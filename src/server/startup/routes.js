7c1dea22ba7da4e4a07e07056d21cd6a09ce8062
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-26 21:27:20 +0800

    Configure cors

--- src/server/startup/routes.js
+++ src/server/startup/routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("../config/corsOption");
 module.exports = function (app) {
   app.use(helmet());
   app.use(cookieParser());
   app.use(express.json());
+  app.use(cors(corsOptions));
   app.use("/api/user", require("../routes/user"));
   app.use("/api/auth", require("../routes/auth"));
   app.use("/refresh", require("../routes/refresh"));
