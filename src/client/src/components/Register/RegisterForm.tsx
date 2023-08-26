67ae502e63bf24cd81f6f90412e8bed34a9046c9
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-26 22:34:33 +0800

    Add confirm password to register form

--- src/client/src/components/Register/RegisterForm.tsx
+++ src/client/src/components/Register/RegisterForm.tsx
@@ -55,6 +55,19 @@ const RegisterForm = () => {
           className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
         />
       </div>
+      <div className="mb-6 pt-3 rounded bg-gray-200">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2 ml-3"
+          htmlFor="passwordConfirm"
+        >
+          Confirm Password
+        </label>
+        <input
+          type="password"
+          id="passwordConfirm"
+          className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+        />
+      </div>
 
       <button
         className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
