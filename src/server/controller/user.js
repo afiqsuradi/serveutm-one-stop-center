877f6d264081ba34869dce2e78b12a204d6fb8c0
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-27 15:04:44 +0800

    Update server respond for user register

--- src/server/controller/user.js
+++ src/server/controller/user.js
@@ -1,30 +1,48 @@
 const { User } = require("../model/user");
 const { validateUserInput } = require("./helper/validation");
 const { createUserInDatabase } = require("./helper/database");
+const jwtCookie = require("./helper/cookie");
 const { generateVerificationToken } = require("./helper/token");
+const { generateAccessToken, generateRefreshToken } = require("./helper/token");
 const { sendMail } = require("./helper/mail");
 
 const userController = {};
 
 userController.validateAndCreateUser = async (req, res) => {
   try {
-    const user = await validateUserInput(req.body);
+    const { success, data, error } = await validateUserInput(req.body);
 
-    if (!user.success) {
-      return res.status(400).send(user.error);
+    if (!success) {
+      return res.status(400).send(error);
     }
 
-    const existingUser = await User.findOne({ email: user.data.email });
+    const existingUser = await User.findOne({ email: data.email });
     if (existingUser) {
       return res.status(400).json({ message: "User already registered" });
     }
 
-    const newUser = await createUserInDatabase(user.data);
+    //generate access token
+    const accessToken = await generateAccessToken(data);
+    //generate refresh token
+    const newRefreshToken = await generateRefreshToken(data);
+    //send refresh token to cookie with httpOnly
+    res.cookie(jwtCookie.name, newRefreshToken, {
+      ...jwtCookie.properties,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    // Add refresh token to user data
+    data.refreshToken = newRefreshToken;
+
+    const newUser = await createUserInDatabase(data);
     const token = await generateVerificationToken(newUser._id);
+
     sendMail(newUser.email, token.token);
-    return res.sendStatus(200);
+    return res.status(200).json({
+      isVerified: newUser.isVerified,
+      accessToken: accessToken,
+    });
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: `"${error.message}"` });
   }
 };
 
