48089d1abd71b5b5767dd2205722174925bac3e8
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-29 00:13:41 +0800

    Added logout functionality

--- src/client/src/components/Layout.tsx
+++ src/client/src/components/Layout.tsx
@@ -1,18 +1,33 @@
 import { Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { defaultAuthValue } from "../context/authProvider";
+import apiClient from "../services/apiClient";
 
 const Layout = () => {
   const { Auth, setAuth } = useAuth();
 
-  const logOut = () => {
+  const logOut = async () => {
     setAuth(defaultAuthValue);
+    try {
+      await apiClient.get("/logout", {
+        withCredentials: true,
+      });
+    } catch (err) {
+      console.error(err);
+    }
   };
+  console.log(Auth);
   return (
     <>
       <header>
         {Auth.username !== "" ? (
-          <button onClick={logOut}>Logout</button>
+          <button
+            onClick={() => {
+              void logOut();
+            }}
+          >
+            Logout
+          </button>
         ) : (
           <></>
         )}
