89c1461486ffaa406c9b4df87dc543946b2acc37
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-30 17:01:02 +0800

    Change domain

--- src/server/controller/helper/mail.js
+++ src/server/controller/helper/mail.js
@@ -3,7 +3,7 @@ const { transportMail } = require("../../utils/mail");
 
 async function sendVerifyMail(email, url, token) {
   return await transportMail.sendMail({
-    from: "no-reply@serveUTM.com",
+    from: "no-reply@serveutm.online",
     to: email,
     subject: "Verify Your Email",
     html: `<a href=\"${url}/verify-confirm?token=${token}\">Verify Email</a>`,
