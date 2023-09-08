56690e66dc602dfbbda673f0e8308b7d4fbef63e
Aiman Afiq <aimanafiq05@gmail.com>
2023-09-08 16:29:37 +0800

    add ssl on react dev environment

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: "./certificate/serveutm-privateKey.key",
+      cert: "./certificate/serveutm.crt",
+    },
     watch: {
       usePolling: true,
     },
