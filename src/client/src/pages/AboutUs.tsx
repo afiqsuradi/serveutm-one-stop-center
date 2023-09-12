02d054153e4931f7923ee6b457a1cb9e1d8634c8
Naza Nezam <nazarudeen.nezam@gmail.com>
2023-09-12 23:58:05 +0800

    Update About Us

--- src/client/src/pages/AboutUs.tsx
+++ src/client/src/pages/AboutUs.tsx
@@ -1,9 +1,14 @@
 import { Container } from "@chakra-ui/react";
 import TeamCard from "../components/About/TeamCard";
+import Introduction from "../components/About/Introduction";
+import Vision from "../components/About/vision";
+
 
 const AboutUs = () => {
   return (
     <Container maxW={"100%"} padding={"0"}>
+      <Introduction/>
+      <Vision />
       <TeamCard />
     </Container>
   );
