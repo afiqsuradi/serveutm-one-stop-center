08c554bc16cc5e5fa4e416bfa5177511a8c03b8d
Aiman Afiq <aimanafiq05@gmail.com>
2023-09-30 20:04:59 +0800

    Change admin profile route

--- src/admin/src/constants/routes.ts
+++ src/admin/src/constants/routes.ts
@@ -1,7 +1,7 @@
 export default {
   DASHBOARD: "/",
   LOGIN: "/login",
-  PROFILE: "/dashboard/profile",
+  PROFILE: "/profile",
   PROFILE_SETTING: "/settings/account",
   USERS: "/dashboard/users",
   EDIT_USER: "/dashboard/users/:username",
--- src/admin/src/partials/Sidebar.tsx
+++ src/admin/src/partials/Sidebar.tsx
@@ -226,22 +226,6 @@ const Sidebar: React.FC<SidebarProps> = ({ sidebarOpen, setSidebarOpen }) => {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to={routes.PROFILE}
-                              className={({ isActive }) =>
-                                "block transition duration-150 truncate " +
-                                (isActive
-                                  ? "text-indigo-500"
-                                  : "text-slate-400 hover:text-slate-200")
-                              }
-                            >
-                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Profile
-                              </span>
-                            </NavLink>
-                          </li>
-                          <li className="mb-1 last:mb-0">
-                            <NavLink
-                              end
                               to="/"
                               className={({ isActive }) =>
                                 "block transition duration-150 truncate " +
