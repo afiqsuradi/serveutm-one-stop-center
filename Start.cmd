e14080c877e0160d9e37f81225231d1dba4f288b
Aiman Afiq <aimanafiq05@gmail.com>
2023-09-30 16:43:47 +0800

    improve start

--- Start.cmd
+++ Start.cmd
@@ -3,13 +3,19 @@
 echo Starting Database...
 start "Database" cmd /k C: ^& mongod
 
+timeout /t 5
+
 echo Starting server...
-start "Server" cmd /k cd ./src/server ^& node ./index.js
+start "Server" cmd /k cd ./src/server ^& code ./ ^& node ./index.js
+
+timeout /t 5
 
 echo Starting client... 
-start "Client" cmd /k cd ./src/client ^& npm run dev
+start "Client" cmd /k cd ./src/client ^& code ./ ^& npm run dev
+
+timeout /t 5
 
 echo Starting admin...
-start "Admin" cmd /k cd ./src/admin ^& npm run dev
+start "Admin" cmd /k cd ./src/admin ^& code ./ ^& npm run dev
 
 echo Done!
