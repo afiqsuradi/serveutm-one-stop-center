6c06eb48a0d5c3ddca0856f5bd0638301ba60e8c
Aiman Afiq <aimanafiq05@gmail.com>
2023-10-05 17:54:39 +0800

    Change wrapper style

--- src/client/src/components/Seller/AddGigWrapper.tsx
+++ src/client/src/components/Seller/AddGigWrapper.tsx
@@ -7,7 +7,7 @@ type Props = {
 const AddGigWrapper = ({ children, title }: Props) => {
   return (
     <div className="border-2 max-w-[85%] mx-auto px-10">
-      <h1 className="my-2 text-xl font-bold">{title}</h1>
+      <h1 className="my-4 text-3xl font-bold">{title}</h1>
       <div className="grid md:grid-cols-[1fr_2fr] md:gap-8">{children}</div>
     </div>
   );
