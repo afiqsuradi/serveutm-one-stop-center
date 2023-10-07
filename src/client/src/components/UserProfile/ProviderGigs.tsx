b804b4ea95cdabb8d8b0a00de3ba2cfe000cf8e5
Aiman Afiq <aimanafiq05@gmail.com>
2023-10-07 18:02:40 +0800

    add route to add gig in user profile

--- src/client/src/components/UserProfile/ProviderGigs.tsx
+++ src/client/src/components/UserProfile/ProviderGigs.tsx
@@ -1,7 +1,10 @@
 import { Card, CardBody, CardHeader, Heading, Tag } from "@chakra-ui/react";
 import { AiFillPlusCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import ROUTES from "../../constants/path";
 
 const ProviderGigs = () => {
+  const navigate = useNavigate();
   return (
     <Card flex={2} align="center" textAlign="center">
       <CardHeader minW="100%" display="flex">
@@ -16,6 +19,13 @@ const ProviderGigs = () => {
           minH="11em"
           maxW="max-content"
           variant="secondary"
+          className="hover:cursor-pointer transition-all"
+          _hover={{
+            transform: "scale(1.1)",
+          }}
+          onClick={() => {
+            navigate(ROUTES.PROVIDER_ADD);
+          }}
         >
           <CardBody
             display="flex"
