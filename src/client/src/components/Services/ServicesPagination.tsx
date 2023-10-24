873c6a5cd407d3b119e259a22f6d9fc71571dee7
Aiman Afiq <aimanafiq05@gmail.com>
2023-10-24 16:27:49 +0800

    added services pagination

---
+++ src/client/src/components/Services/ServicesPagination.tsx
@@ -0,0 +1,58 @@
+import { ServicesFilterType } from "@/interface/Service";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "../ui/button";
+
+interface Props {
+  count: number;
+  setFilters: React.Dispatch<React.SetStateAction<ServicesFilterType>>;
+  limit: number;
+  page: number;
+}
+
+const ServicesPagination = ({ count, setFilters, limit, page }: Props) => {
+  const pages = Math.ceil(count / limit);
+  return (
+    <>
+      <div className="flex justify-end items-center gap-4">
+        <h4>Limit: </h4>
+        <Select>
+          <SelectTrigger className="w-[110px]">
+            <SelectValue placeholder="Card limit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="5">5</SelectItem>
+              <SelectItem value="10">10</SelectItem>
+              <SelectItem value="20">20</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="flex justify-center">
+        {Array.from({ length: pages }, (_, i) => (
+          <Button
+            key={i}
+            variant={`${i === page - 1 ? "outline" : "default"}`}
+            className={"w-10"}
+            onClick={() => {
+              setFilters((prev) => {
+                return { ...prev, page: i + 1 };
+              });
+            }}
+          >
+            {i + 1}
+          </Button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ServicesPagination;
--- src/client/src/pages/ServicesList.tsx
+++ src/client/src/pages/ServicesList.tsx
@@ -11,11 +11,13 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { FaExclamation } from "react-icons/fa";
 import ServicesNotFound from "@/components/Services/ServicesNotFound";
 import { useLocation } from "react-router-dom";
+import ServicesPagination from "@/components/Services/ServicesPagination";
+import ServicesSearchBar from "@/components/Services/ServicesSearchBar";
 
 const ServicesList = () => {
   const location = useLocation();
   const [services, setServices] = useState<ServiceType[] | null>(null);
-  const [filters, setFilters] = useState<ServicesFilterType>({ limit: 8 });
+  const [filters, setFilters] = useState<ServicesFilterType>({ limit: 5 });
   const search = new URLSearchParams(location.search).get("search") || "";
   const type = new URLSearchParams(location.search).get("type") || "";
   const { data, isLoading, error } = useServices(filters);
@@ -49,39 +51,52 @@ const ServicesList = () => {
   }, [data]);
   if (isLoading) return <Spinner />;
   return (
-    <div className="container space-y-6 py-12">
-      {error && (
-        <Alert className="text-start" variant={"destructive"}>
-          <FaExclamation />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>{error}</AlertDescription>
-        </Alert>
-      )}
-      <div className="space-y-4 border p-6">
-        <h1 className="text-xl font-semibold">
-          {filters.type &&
-          filters.type.length > 0 &&
-          filters.textInput &&
-          filters.textInput.length > 0
-            ? `Result for ${filters.textInput}`
-            : "Recently Added"}
-        </h1>
-        <div className="flex gap-6 flex-wrap">
-          {services &&
-            services.map((service) => {
-              return (
-                <ServiceCard
-                  key={service._id}
-                  service={service}
-                  isOwner={false}
-                  variant="secondary"
-                />
-              );
-            })}
-          {services && data && data.count === 0 && <ServicesNotFound />}
+    <>
+      <div className="py-4 border-b">
+        <div className="container">
+          <ServicesSearchBar />
         </div>
       </div>
-    </div>
+      <div className="container space-y-6 py-12">
+        {error && (
+          <Alert className="text-start" variant={"destructive"}>
+            <FaExclamation />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>{error}</AlertDescription>
+          </Alert>
+        )}
+        <div className="space-y-4 border p-6">
+          <h1 className="text-xl font-semibold">
+            {filters.type &&
+            filters.type.length > 0 &&
+            filters.textInput &&
+            filters.textInput.length > 0
+              ? `Result for ${filters.textInput}`
+              : "Recently Added"}
+          </h1>
+          <div className="flex gap-6 flex-wrap">
+            {services &&
+              services.map((service) => {
+                return (
+                  <ServiceCard
+                    key={service._id}
+                    service={service}
+                    isOwner={false}
+                    variant="secondary"
+                  />
+                );
+              })}
+            {services && data && data.count === 0 && <ServicesNotFound />}
+          </div>
+          <ServicesPagination
+            count={data?.count || 0}
+            setFilters={setFilters}
+            limit={filters.limit}
+            page={filters.page || 1}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
